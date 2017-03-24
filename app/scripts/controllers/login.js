6afb3769536fadf7ae10a658b57f95308fd6ad1a
xiaxiaonan <1963791252@qq.com>
2017-03-24 09:33:57 +0800

    3.24 9:33      轮播图完善      时间补零      svg删除             孙夏楠

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -99,3 +99,13 @@ angular.module('hotelApp')
 
 		}
 	}])
+	.directive("qx",function(){
+		return{
+			restrict:"ECAM",
+			template:'<div id="carousel-example-generic" class="carousel slide fs-lunbo" data-ride="carousel" data-interval="2500"><ol class="carousel-indicators" style="display: none;"><li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li><li data-target="#carousel-example-generic" data-slide-to="1"></li><li data-target="#carousel-example-generic" data-slide-to="2"></li><li data-target="#carousel-example-generic" data-slide-to="3"></li><li data-target="#carousel-example-generic" data-slide-to="4"></li><li data-target="#carousel-example-generic" data-slide-to="5"></li><li data-target="#carousel-example-generic" data-slide-to="6"></li></ol><div class="carousel-inner" role="listbox"><div class="item active"><img src="images/f1.jpg" alt="..."></div><div class="item"><img src="images/f2.jpg" alt="..."></div><div class="item"><img src="images/f3.jpg" alt="..."></div><div class="item"><img src="images/f4.jpg" alt="..."></div><div class="item"><img src="images/f5.jpg" alt="..."></div><div class="item"><img src="images/f8.jpg" alt="..."></div><div class="item"><img src="images/f7.jpg" alt="..."></div></div></div>',
+			replace:true,
+			link:function(scope,e,attr){
+				e.carousel()
+			}
+		}
+	})
